58cc18ca4bfa815e3845bc3c9e4af283925671dd
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2021-09-18 11:52:33 -0400

    Disable template modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config.module';
-import { GraphQLModule } from './graphql.module';
-import { MessageModule } from './message/message.module';
-import { PubSub } from './pub-sub';
 
 @Module({
-	imports: [GraphQLModule, ConfigModule, MessageModule],
-	providers: [PubSub],
+	imports: [ConfigModule],
+	providers: [],
 })
 export class AppModule {}
